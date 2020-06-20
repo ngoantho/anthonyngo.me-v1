7972da5cfc526d173ed5cb6f542bfb0734a4e2e3
Anthony Ngo <anthonyngo002@gmail.com>
2020-06-20 10:30:57 -0700

    Revamp indexes - starting fresh

--- src/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-unused-vars -- ignore h, Fragment */
-import { h, Fragment, render } from "preact";
-const App = () => (
-  <>
-    <h1>Wow!</h1>
-  </>
-);
-render(<App />, document.body);
