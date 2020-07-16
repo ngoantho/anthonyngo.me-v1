194ab95c45aee03a45047044928d9bc36646d41e
Anthony Ngo <anthonyngo002@gmail.com>
2020-07-16 11:38:53 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import { Landing } from "components/sections";
 // import { Layout } from "components";
 import dynamic from "next/dynamic";
+import Head from 'next/head';
 
 const Layout = dynamic(() => import("components/layout"), {
   ssr: true,
@@ -9,6 +10,10 @@ const Layout = dynamic(() => import("components/layout"), {
 export default function () {
   return (
     <Layout>
+      <Head>
+        <title>Anthony Ngo</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Landing id="landing" />
     </Layout>
   );
